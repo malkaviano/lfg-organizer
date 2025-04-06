ef60c231bb03eb91ebce7b9ff72a2f73390fe011
Rafael Karayannopoulos <rafael.kss@gmail.com>
2025-04-06 12:19:41 -0300

    add scheduler module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GroupOrganizerModule } from '@/group/group-organizer.module';
 import { DungeonModule } from '@/dungeon/dungeon.module';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
-  imports: [DungeonModule, GroupOrganizerModule],
+  imports: [ScheduleModule.forRoot(), DungeonModule, GroupOrganizerModule],
   controllers: [AppController],
   providers: [AppService],
 })
