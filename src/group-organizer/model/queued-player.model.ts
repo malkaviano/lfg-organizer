86e01c7f90db1c8e75c799f2e02501c952b19130
Rafael Karayannopoulos <rafael.kss@gmail.com>
2025-04-27 09:41:42 -0300

    queued player model changed

--- src/group-organizer/model/queued-player.model.ts
+++ src/group-organizer/model/queued-player.model.ts
@@ -1,55 +1,36 @@
-import { Entity, PrimaryKey } from '@mikro-orm/core';
+import { Entity, PrimaryKey, Property } from '@mikro-orm/core';
 
 import { DungeonName } from '@/dungeon/dungeon-name.literal';
 import { PlayerRole } from '@/dungeon/player-role.literal';
 import { PlayerLevel } from '@/dungeon/player-level.literal';
 import { PlayerStatus } from '@/group/player-status.literal';
 
-@Entity()
+@Entity({ tableName: 'queued_players' })
 export class QueuedPlayerModel {
-  @PrimaryKey({ autoincrement: true })
-  public id!: BigInt;
+  @PrimaryKey({ type: 'string' })
+  public id!: string;
 
-  public level: PlayerLevel;
+  @Property({ type: 'smallint' })
+  public level!: PlayerLevel;
 
-  public roles: PlayerRole[];
+  @Property({ type: 'string[]' })
+  public roles!: PlayerRole[];
 
-  public dungeons: DungeonName[];
+  @Property({ type: 'string[]' })
+  public dungeons!: DungeonName[];
 
-  public queuedAt: string;
+  @Property({ type: 'string' })
+  public queuedAt!: string;
 
-  public status: PlayerStatus;
+  @Property({ type: 'string' })
+  public status!: PlayerStatus;
 
-  public playingWith: string[];
+  @Property({ type: 'string[]' })
+  public playingWith!: string[];
 
+  @Property({ type: 'string' })
   public groupId?: string;
 
+  @Property({ type: 'string' })
   public groupedAt?: string;
-
-  constructor(
-    args: {
-      readonly level: PlayerLevel;
-      readonly roles: PlayerRole[];
-      readonly dungeons: DungeonName[];
-      readonly queuedAt: string;
-      readonly status: PlayerStatus;
-      readonly playingWith: string[];
-    } = {
-      level: 10,
-      roles: [],
-      dungeons: [],
-      queuedAt: '',
-      status: 'WAITING',
-      playingWith: [],
-    }
-  ) {
-    const { level, roles, dungeons, queuedAt, status } = args;
-
-    this.level = level;
-    this.roles = roles;
-    this.dungeons = dungeons;
-    this.queuedAt = queuedAt;
-    this.status = status;
-    this.playingWith = args.playingWith;
-  }
 }
